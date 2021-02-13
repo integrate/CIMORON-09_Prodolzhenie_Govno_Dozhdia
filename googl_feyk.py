d42ea9368abad1a377ca9aade96ffa240693e806
levon <airtk@ya.ru>
2021-02-13 12:41:00 +0300

    ble ble blee ble просто пипец но это не точно

--- googl_feyk.py
+++ googl_feyk.py
@@ -1,9 +1,18 @@
 import pygame
-from pygame import display
+from pygame import display,event
+
 # def orabotka_sobity():]
 def risovanie():
-    y.fill([255,60,90])
+    y.fill([255, 60, 90])
     display.flip()
-while 1==1:
-    y=display.set_mode([500,800])
+
+
+def obrabotka():
+
+    if did.type == pygame.QUIT:
+        exit()
+        event
+
+while 1 == 1:
+    y = display.set_mode([500, 800])
     risovanie()
