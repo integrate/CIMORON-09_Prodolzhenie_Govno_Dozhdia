ff89953948b9ca7becba3c91a38488da1a8f1402
levon <airtk@ya.ru>
2021-02-20 11:59:45 +0300

    сегодня нечего неполучмдо

--- googl_feyk.py
+++ googl_feyk.py
@@ -1,17 +1,21 @@
 import pygame
-from pygame import display,event
+from pygame import display, event
+
 
 # def orabotka_sobity():]
 def risovanie():
     y.fill([255, 60, 90])
     display.flip()
-def obrabotka():
 
+
+def obrabotka():
     lodka = pygame.event.get()
     for did in lodka:
         if did.type == pygame.QUIT:
+
             exit()
 
 while 1 == 1:
     y = display.set_mode([500, 800])
+    obrabotka()
     risovanie()
