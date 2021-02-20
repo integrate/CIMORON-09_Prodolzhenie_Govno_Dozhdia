eb794ef396b7fab9efd7b0c7999177e2c0db2a57
levon <airtk@ya.ru>
2021-02-20 12:42:50 +0300

    сегодня нечего неполучмдо

--- googl_feyk.py
+++ googl_feyk.py
@@ -1,12 +1,13 @@
 import pygame
 from pygame import display, event
-
+platforma=pygame.Rect(200,200,100,100)
 
 # def orabotka_sobity():]
 def risovanie():
     y.fill([255, 60, 90])
-    display.flip()
-
+    rtyu = [200, 120, 10]
+    pygame.draw.rect(y,rtyu,platforma)
+    display.flip() #Показывает окно пользователю
 
 def obrabotka():
     lodka = pygame.event.get()
@@ -14,8 +15,14 @@ def obrabotka():
         if did.type == pygame.QUIT:
 
             exit()
+    keys=pygame.key.get_pressed()
+    if keys[pygame.K_a]:
+        print("a")
+
 
 while 1 == 1:
     y = display.set_mode([500, 800])
     obrabotka()
     risovanie()
+
+
